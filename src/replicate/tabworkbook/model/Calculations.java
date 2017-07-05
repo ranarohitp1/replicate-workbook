9fe2a8d7196d857071e59d191fda3ac35c7d0ee5
ranarohitp1 <rana.rohit148@gmail.com>
2017-07-05 19:50:12 +0530

    Add files via upload
    added table calculations part

---
+++ src/replicate/tabworkbook/model/Calculations.java
@@ -0,0 +1,74 @@
+package replicate.tabworkbook.model;
+
+public class Calculations {
+
+	String caption;
+	String datatype;
+	String name;
+	String role;
+	String type;
+	String cal_Class;
+	String formula;
+	
+	public Calculations()
+	{
+		
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public String getDatatype() {
+		return datatype;
+	}
+
+	public void setDatatype(String datatype) {
+		this.datatype = datatype;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCal_Class() {
+		return cal_Class;
+	}
+
+	public void setCal_Class(String cal_Class) {
+		this.cal_Class = cal_Class;
+	}
+
+	public String getFormula() {
+		return formula;
+	}
+
+	public void setFormula(String formula) {
+		this.formula = formula;
+	}
+	
+}
