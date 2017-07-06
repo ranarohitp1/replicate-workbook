b45ec722e4ce3592d6a12259e54b3394dce0d7cb
ranarohitp1 <rana.rohit148@gmail.com>
2017-07-06 02:53:54 -0700

    Add files via upload

---
+++ src/replicate/tabworkbook/service/XmlConstants.java
@@ -0,0 +1,44 @@
+package replicate.tabworkbook.service;
+
+public class XmlConstants {
+
+	public final static String AUTHENTICATION = "authentication";
+	public final static String SERVER = "server";
+	public final static String DBNAME = "dbname";
+	public final static String USERNAME = "username";
+	
+	public final static String DATASOURCE = "datasource";
+	public final static String HASCONNECTION = "hasconnection";
+	public final static String CONNECTION = "connection";
+	public final static String NAME = "name";
+	public final static String WORKSHEET = "worksheet";
+	public final static String RELATION = "relation";
+	public final static String TABLE = "table";
+	public final static String COLUMN = "column";
+	public final static String CAPTION ="caption";
+	public final static String DATATYPE = "datatype";
+	public final static String ROLE = "role";
+	public final static String VALUE = "value";
+	
+	
+	public final static String PANE = "pane";
+	public final static String ID = "id";
+	public final static String Y_AXIS_NAME = "y-axis-name";
+	public final static String X_AXIS_NAME = "x-axis-name";
+	public final static String BREAKDOWN = "breakdown";
+	public final static String _CLASS = "class";
+	public final static String COLOR = "color";
+	public final static String MAP = "map";
+	public final static String TO = "to";
+	public final static String MARK = "mark";
+	
+	public final static String BUCKET = "bucket";
+	public final static String ENCODING = "encoding";
+	public final static String ATTR = "attr";
+	public final static String FIELD = "field";
+	
+	public final static String TYPE = "type";
+	public final static String CALCULATION = "calculation";
+	public final static String FORMULA = "formula";
+	public final static String COLOR_PALETTE = "color-palette";
+}
